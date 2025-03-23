463330475ae360890ffaf2d7e53a7d33ed450a4b
d0m1n1qu3 <d0m1n1qu3@l3x.de>
2025-03-23 20:11:04 +0100

    make the buttons more colorful
    Signed-off-by: d0m1n1qu3 <d0m1n1qu3@l3x.de>

--- SRC/ShineWiFi-ModBus/Index.h
+++ SRC/ShineWiFi-ModBus/Index.h
@@ -61,6 +61,33 @@ copies or substantial portions of the Software. -->
             font-size: .8rem;
             text-align: center;
         }
+
+        .blue {
+            border: solid 1px rgb(95, 166, 202);
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.4);
+            -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            -moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            background:rgb(43, 149, 185);
+        }
+
+        .yellow {
+            border: solid 1px rgb(202, 195, 95);
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.4);
+            -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            -moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            background:rgb(185, 185, 43);
+        }
+       
+        .red {
+            border: solid 1px rgb(202, 95, 95);
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.4);
+            -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            -moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.4), 0 1px 1px rgba(0, 0, 0, 0.2);
+            background:rgb(185, 43, 43);
+        }
     </style>
 </head>
 
@@ -68,17 +95,18 @@ copies or substantial portions of the Software. -->
     <h2>Growatt Inverter</h2>
 
     <div><canvas id="powerChart"></canvas></div>
-    <div id="DataContainer"></div>
 
     <div class="linkButtonBar">
         <a href="./status" class="linkButton">Json</a>
         <a href="./uiStatus" class="linkButton">UI Json</a>
         <a href="./metrics" class="linkButton">Metrics</a>
         <a href="./debug" class="linkButton">Log</a>
-        <a onClick="return confirm('Starting config AP will disconnect you from the device. Are you sure?');" href="./startAp" class="linkButton">Start Config AP</a>
-        <a onClick="return confirm('This will reboot the Wifi Stick. Are you sure?');" href="./reboot" class="linkButton">Reboot</a>
-        <a href="./postCommunicationModbus" class="linkButton">RW Modbus</a>
+        <a onClick="return confirm('Starting config AP will disconnect you from the device. Are you sure?');" href="./startAp" class="linkButton blue">Start Config AP</a>
+        <a onClick="return confirm('This will reboot the Wifi Stick. Are you sure?');" href="./reboot" class="linkButton yellow">Reboot</a>
+        <a href="./postCommunicationModbus" class="linkButton red">RW Modbus</a>
     </div>
+
+    <div id="DataContainer"></div>
 
 <script>
     let initialised = false;
